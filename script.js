80555890825dc2028dff0923ebb579be0cdc6ae2
jajones49 <143618797+jajones49@users.noreply.github.com>
2024-05-13 15:50:53 +0000

    Pending changes exported from your codespace

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 // Declare your arrays here
+let Chips=["l.jpg", "ruffles.jpg", "hotfries.jpg"];
+let Candy=["kk.jpg", "ab.jpg", "t.jpg"];
+let Beverage=["water.jpg", "ga.jpg", "sl.jpg"];
+
+let choices
+
 
 // Make sure to declare your HTML elements as variables! 
 
+
 // Submit Button 
-let submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {});
+let submitButton = document.querySelector(".submit");
+let input = document.querySelector("input");
+let suggestions=document.querySelector(".suggestions");
+
+submitButton.addEventListener("click", function()  {
+
+    choices =input.value;
+
+
+if (choices==="Chips")
+    {choices=Chips;}
+
+else if (choices==="Candy")
+    {choices=Candy;}
+
+else if (choices==="Beverage")
+    {choices=Beverage;}
+
+
+
+
+choices.forEach(function(choice){
+    let picture=document.createElement("img");
+    console.log(picture);
+    picture.src=choice;
+    suggestions.appendChild(picture);
+    });
+    
+});
+
+
